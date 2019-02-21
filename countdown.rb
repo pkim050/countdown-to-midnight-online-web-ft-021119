bbefba8f6736a1775113b1dd2fa4df363c2f9ca3
Patrick Kim <pkim050@gmail.com>
2019-02-21 05:13:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,10 @@ def countdown(integer)
   return "HAPPY NEW YEAR!"
 end
 
-def 
+def countdown_with_sleep(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    end
+  return "HAPPY NEW YEAR!"
+end
